88235e827d00398781228f96bb5ede1ff58ef843
Laure Fischer <laurefischer27@gmail.com>
2014-01-15 10:59:28 -0500

    homework review through email address

--- main.rb
+++ main.rb
@@ -2,11 +2,11 @@ require_relative 'source'
 require 'pry'
 
 def valid_ip_addresses(string)
-  string.scan(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/)
+  string.scan(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/) # Can use \d instead of [0-9] - will look for any digit
 end
 
 def valid_mit_ip_addresses(string)
-  string.scan(/[1][8]\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)
+  string.scan(/[1][8]\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/) # Can remove brackets from 1 and 8, same thing
 end
 
 def non_mit_ip_addresses(string)
@@ -14,19 +14,19 @@ def non_mit_ip_addresses(string)
 end
 
 def valid_phone_numbers(string)
-  string.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
+  string.scan(/[2-9]\d{0,2}-\d{0,3}-?\d{4}/).uniq # question mark means thing immediately preceding it is optional
 end
 
 def area_codes(string)
-  string.scan(/[2-9][0-9][0-9](?=\-[0-9]{3}\-[0-9]{4})/).uniq
+  string.scan(/[2-9]\d{2}(?=-[0-9]{3}-[0-9]{4})/).uniq
 end
 
 def email_addresses(string)
-  string.scan(/\w+@\w+\.com/)
+  string.scan(/\w+@\w+\.com/) # word characters are letters, numbers, underscores
 end
 
 def zip_codes(string)
-  string.scan(/(?<=\D)[0-9]{5}(?=\D)/)
+  string.scan(/(?<=\D)\d{5}(?=\D)/)
 end
 
 def hex_colors(string)
