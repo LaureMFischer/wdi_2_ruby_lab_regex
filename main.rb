4adddd25161a011784b9f296a46b20d351c54502
Laure Fischer <laurefischer27@gmail.com>
2014-01-14 19:59:01 -0500

    completed valid_ip_addresses and valid_phone_numbers methods

--- main.rb
+++ main.rb
@@ -1,26 +1,30 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses 
-end 
-
-def valid_mit_ip_addresses 
+def valid_ip_addresses(string)
+  string.scan(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/)
 end
 
-def non_mit_ip_addresses 
-end 
-
-def valid_phone_numbers 
+def valid_mit_ip_addresses(string)
+  string.scan(/\b(?:18{1}\.){1}([0-9]{1,3}\.){2}([0-9]{1,3})\b/)
+ # string.scan(/\b(18{1}\.){1}([0-9]{1,3}\.){2}[0-9]{1,3}\b/)
 end
 
-def area_codes 
+def non_mit_ip_addresses
 end
 
-def email_addresses 
+def valid_phone_numbers(string)
+  string.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
 end
 
-def zip_codes 
+def area_codes
 end
 
-def hex_colors 
+def email_addresses
+end
+
+def zip_codes
+end
+
+def hex_colors
 end
